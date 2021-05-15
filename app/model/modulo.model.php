7338aef8a1e37f54168fe7c95c19b4da19b12313
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-05-15 09:37:18 -0300

    Início Models

---
+++ app/model/modulo.model.php
@@ -0,0 +1,53 @@
+<?php
+
+    class Modulo {
+        private $id;
+        private $descricao;
+        private $sigla;
+        private $dataCadastro;
+        private $usuarioResponsavel;
+
+        
+        // getters e setters 
+        public function getId() {
+            return $this->id;
+        }
+        
+        public function setId($id) {
+            $this->id = $id;
+        }
+
+        public function getDescricao() {
+            return $this->descricao;
+        }
+        
+        public function setDescricao($descricao) {
+            $this->descricao = $descricao;
+        }
+
+        public function getSigla() {
+            return $this->sigla;
+        }
+        
+        public function setSigla($sigla) {
+            $this->sigla = $sigla;
+        }
+
+        public function getDataCadastro() {
+            return $this->dataCadastro;
+        }
+        
+        public function setDataCadastro($dataCadastro) {
+            $this->dataCadastro = $dataCadastro;
+        }
+
+        public function getUsuario() {
+            return $this->usuarioResponsavel;
+        }
+        
+        public function setUsuarioResponsavel($usuarioResponsavel) {
+            $this->usuarioResponsavel = $usuarioResponsavel;
+        }
+    }
+
+?>
