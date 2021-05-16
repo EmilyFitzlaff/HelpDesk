293a7edc1d6944ecf3a593bc83920a959e8846f1
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-05-16 20:46:58 -0300

    criação model ChamadoTipo

--- app/model/chamadoTipo.model.php
+++ app/model/chamadoTipo.model.php
@@ -1,7 +1,42 @@
 <?php
 
     class ChamadoTipo {
+        private $id;
+        private $descricao;
+        private $observacao;
+        private $dataCadastro;
+        
+        // getters e setters 
+        public function getId() {
+            return $this->id;
+        }
+        
+        public function setId($id) {
+            $this->id = $id;
+        }
 
+        public function getDescricao() {
+            return $this->descricao;
+        }
+        
+        public function setDescricao($descricao) {
+            $this->descricao = $descricao;
+        }
+
+        public function getObservacao() {
+            return $this->observacao;
+        }
+        
+        public function setObservacao($observacao) {
+            $this->observacao = $observacao;
+        }
+
+        public function getDataCadastro() {
+            return $this->dataCadastro;
+        }
+        
+        public function setDataCadastro($dataCadastro) {
+            $this->dataCadastro = $dataCadastro;
+        }
     }
-
 ?>
