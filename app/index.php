70d573915d28b0e3b3477e51b5999c8a80e82148
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-06-01 20:29:01 -0300

    Atualizações

---
+++ app/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+    <?php
+        $title = "Página Principal";
+
+        require_once('lib/head.php');
+    ?>
+
+    <body>
+        <div class='container-fluid'>
+            <?php
+                require_once('lib/menu.php');
+            ?>
+        </div>
+    </body>
+</html>
