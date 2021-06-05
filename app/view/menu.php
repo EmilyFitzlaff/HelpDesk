5e0c2ee8ba8b8976ca0947f10a3620959ff0275d
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-06-05 10:47:00 -0300

    Desenvolvimento view home

--- app/view/menu.php
+++ app/view/menu.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color: #e3f2fd;">
     <div class="container-fluid">
-        <a class="navbar-brand" href="home.view.php">Helpdesk</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <a class="navbar-brand" href="home.view.php">
+            <img src="img/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            Helpdesk
+        </a>
         <div class="collapse navbar-collapse" id="navbarScroll">
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">                
                 <li class="nav-item dropdown">
